667a8604912392b68dc18cc0d8c1ca148f18248a
Viktor Voscek <247199@vutbr.cz>
2024-04-04 16:39:39 +0200

    CV09 - initial commit

---
+++ CV09/Calculator.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV09
+{
+
+
+    internal class Calculator
+    {
+        public string Display = "";
+        public string Pamet = "";
+        private string Operation = "+";
+        private double PrveCislo = 0;
+        private double DruheCislo = 0;
+        private double Vysledok = 0;
+        private enum Stav
+        {
+            PrvniCislo,
+            Operace,
+            DruheCislo,
+            Vysledek
+        };
+        Stav _stav;
+
+        public Calculator()
+        {
+            Stav _stav = Stav.PrvniCislo;
+        }
+
+        
+        private void Calculate()
+        {
+            switch(Operation)
+            {
+                case "+":
+                    Vysledok = PrveCislo + DruheCislo; break;
+                case "-":
+                    Vysledok = PrveCislo - DruheCislo; break;
+                case "*":
+                    Vysledok = PrveCislo * DruheCislo; break;
+                case "/":
+                    Vysledok = PrveCislo / DruheCislo; break;
+            }
+        }
+
+        public void Tlacitko(string s)
+        {
+            // rozhodovanie na zaklade _stav
+            switch (_stav)
+            {
+                case Stav.PrvniCislo:
+                    ActionsPrvniCislo(s); break;
+
+                case Stav.Operace:
+                    ActionsOperace(s); break;
+
+                case Stav.DruheCislo:
+                    ActionsDruheCislo(s); break;
+
+                case Stav.Vysledek:
+                    ActionsVysledek(s); break;
+            }
+        }
+
+        private void ActionsVysledek(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                case ",":
+                    Display = s; _stav = Stav.PrvniCislo; break;
+                case "back":
+                    Display = ""; _stav = Stav.PrvniCislo; break;
+                case "CE":
+                    Display = ""; _stav = Stav.PrvniCislo; break;
+                case "MC":
+                    Pamet = ""; break;
+                case "MR":
+                    Display = Pamet; break;
+                case "M+":
+                    Pamet = (Double.Parse(Pamet) + Double.Parse(Display)).ToString(); break;
+                case "M-":
+                    Pamet = (Double.Parse(Pamet) - Double.Parse(Display)).ToString(); break;
+                case "MS":
+                    Pamet = Display; break;
+                default:
+                    Console.WriteLine("Operator nema v tomto rezime ziadnu funkciu."); break;
+            }
+        }
+
+        private void ActionsDruheCislo(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                case ",":
+                    Display += s; break;
+                case "back":
+                    Display = Display.Remove(Display.Length - 1); break;
+                case "=":
+                    DruheCislo = Double.Parse(Display);
+                    Calculate();
+                    Display = Vysledok.ToString();
+                    _stav = Stav.Vysledek;
+                    break;
+                case "CE":
+                    Display = ""; _stav = Stav.PrvniCislo; break;
+                case "MC":
+                    Pamet = ""; break;
+                case "MR":
+                    Display = Pamet; break;
+                case "M+":
+                    Pamet = (Double.Parse(Pamet) + Double.Parse(Display)).ToString(); break;
+                case "M-":
+                    Pamet = (Double.Parse(Pamet) - Double.Parse(Display)).ToString(); break;
+                case "MS":
+                    Pamet = Display; break;
+                default:
+                    Console.WriteLine("Operator nema v tomto rezime ziadnu funkciu."); break;
+            }
+        }
+
+        private void ActionsOperace(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                case ",":
+                    Display = s; _stav = Stav.DruheCislo; break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    Display = s; Operation = s; break;
+                case "CE":
+                    Display = ""; _stav = Stav.PrvniCislo; break;
+                default:
+                    Console.WriteLine("Operator nema v tomto rezime ziadnu funkciu."); break;
+            }
+        }
+
+        private void ActionsPrvniCislo(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                case ",":
+                    Display += s; break;
+                case "back":
+                    Display = Display.Remove(Display.Length - 1); break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    PrveCislo = Double.Parse(Display);
+                    Display = s; Operation = s;
+                    _stav = Stav.Operace;
+                    break;
+                case "=":
+                    Calculate();
+                    Display = Vysledok.ToString();
+                    _stav = Stav.Vysledek;
+                    break;
+                case "CE":
+                    Display = ""; _stav = Stav.PrvniCislo; break;
+                case "MC":
+                    Pamet = ""; break;
+                case "MR":
+                    Display = Pamet; break;
+                case "M+":
+                    Pamet = (Double.Parse(Pamet) + Double.Parse(Display)).ToString(); break;
+                case "M-":
+                    Pamet = (Double.Parse(Pamet) - Double.Parse(Display)).ToString(); break;
+                case "MS":
+                    Pamet = Display; break;
+                default:
+                    Console.WriteLine("Operator nema v tomto rezime ziadnu funkciu."); break;
+            }
+        }
+    }
+}
---
+++ CV09/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CV09
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator = new Calculator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            display.Content = calculator.Display;
+            pamet.Content = calculator.Pamet;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Tlacitko((sender as Button).Content.ToString());
+            display.Content = calculator.Display;
+            pamet.Content = calculator.Pamet;
+        }
+    }
+}
